aa7bdfa3cd235a2f9d448fc184532b3a3f1f9b89
Oliver Woodings <oliver.woodings@qubitdigital.com>
2014-11-08 17:19:53 +0000

    Start of express server

--- app/app.js
+++ app/app.js
@@ -1,5 +1,13 @@
-var _       = require("lodash");
+var express    = require("express");
+var log        = require("./log");
+var bodyParser = require("body-parser");
 
-var models    = require("./models/index");
-var sequelize = require("./models/sequelize");
-var process   = require("./process");
+var app = express();
+
+app.use(express.static(__dirname + "/../public/build"));
+app.use(bodyParser.json());
+
+var server = app.listen(3000, function () {
+  var address = server.address();
+  log.info("LAN-Auth server started on %s:%s", address.address, address.port);
+});
